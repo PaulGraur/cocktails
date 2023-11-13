157bbd4f5c90cccf51b0d3d328143458a992a1f8
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2023-11-13 17:27:23 +0100

    feature: add header component and style to it

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -8,55 +8,53 @@ import BurgerMenuOpen from "./../images/header/burger-menu.svg";
 
 const HeaderComponent = () => {
   return (
-<header className={classNames(s.container,s.header)}>
-      
-        <div className={s.header__info}>
+    <header className={classNames(s.container, s.header)}>
+      <div className={s.header__info}>
         <Link href="/app/page.tsx" target="_blank">
-<Image src={Logo} alt="logo" width="156" height="43" />
-</Link>
-          <nav className={s.header__nav}>
-            <ul className={s.header__nav__list}>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">Favourite</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div>
-          <form className={s.header__search} action="">
-            <button className={s.header__search__button}>
-              <Image
-                className={s.header__search__svg}
-                src={Search}
-                alt="Search"
-              />
-              <div className={s.header__search__line}></div>
-              <input
-                className={s.header__search__input}
-                type="Search |"
-                autoComplete="off"
-                name="searchQuery"
-                placeholder="Search |"
-              />
-            </button>
-          </form>
-        </div>
-        <div className={s.header__thema}>
-          <p className={s.header__thema__light}>Light</p>
-          <button className={s.header__thema__button}></button>
-          <p className={s.header__thema__dark}>Dark</p>
-        </div>
-        <button className={s.menu__open} type="button">
-          <Image
-            className={s.menu__open__img}
-            src={BurgerMenuOpen}
-            alt="Burger-menu"
-          />
-        </button>
-     
+          <Image src={Logo} alt="logo" width="156" height="43" />
+        </Link>
+        <nav className={s.header__nav}>
+          <ul className={s.header__nav__list}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Favourite</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <form className={s.header__search} action="">
+          <button className={s.header__search__button}>
+            <Image
+              className={s.header__search__svg}
+              src={Search}
+              alt="Search"
+            />
+            <div className={s.header__search__line}></div>
+            <input
+              className={s.header__search__input}
+              type="Search |"
+              autoComplete="off"
+              name="searchQuery"
+              placeholder="Search |"
+            />
+          </button>
+        </form>
+      </div>
+      <div className={s.header__thema}>
+        <p className={s.header__thema__light}>Light</p>
+        <button className={s.header__thema__button}></button>
+        <p className={s.header__thema__dark}>Dark</p>
+      </div>
+      <button className={s.menu__open} type="button">
+        <Image
+          className={s.menu__open__img}
+          src={BurgerMenuOpen}
+          alt="Burger-menu"
+        />
+      </button>
     </header>
   );
 };
