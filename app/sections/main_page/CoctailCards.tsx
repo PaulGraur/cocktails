bb4ef9f342d6ed5b17a09efc0099d67d59756973
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2023-12-15 16:44:15 +0100

    feature:  restore сoctail сards and style to it

--- app/sections/main_page/CoctailCards.tsx
+++ app/sections/main_page/CoctailCards.tsx
@@ -1,14 +1,14 @@
+"use client";
 import Image from "next/image";
-import classNames from "classnames";
 import s from "./CoctailCards.module.scss";
 import MainTitleComponent from "./../../../components/MainTitleComponent";
 import CardsListCoctails from "@/components/CardsListCoctails";
-import  data, {CardData} from "./../../../assets/data";
+import data, { CardData } from "./../../../assets/data";
+import { useState, useEffect } from "react";
 
 const CoctailCards = () => {
-
   const handleButtonClick = () => {
-    console.log('Button clicked!');
+    console.log("Button clicked!");
   };
 
   return (
--- assets/data.ts
+++ assets/data.ts
@@ -1,58 +1,96 @@
+import { StaticImageData } from "next/image";
 import A1 from "./../images/coctailCards/A1.png";
+import AJ from "./../images/coctailCards/AJ.png";
+import ABC from "./../images/coctailCards/ABC.png";
+import Ace from "./../images/coctailCards/Ace.png";
+import ATT from "./../images/coctailCards/ATT.png";
+import ACID from "./../images/coctailCards/ACID.png";
+import Affair from "./../images/coctailCards/Affair.png";
+import Apello from "./../images/coctailCards/Apello.png";
+import AmarettoSunrise from "./../images/coctailCards/AmarettoSunrise.png";
 
 export interface CardData {
-    id: number;
-    imageSrc: string;
-    text: string;
-  }
-  
-  const data: CardData[] = [
-    {
-      id: 1,
-      imageSrc: A1,
-      text: 'Negroni',
+  id: number;
+  imageSrc: {
+    url: StaticImageData;
+    alt: string;
+  };
+  text: string;
+}
+
+const data: CardData[] = [
+  {
+    id: 1,
+    imageSrc: {
+      url: A1,
+      alt: "Negroni Image",
     },
-    {
-      id: 2,
-      imageSrc: A1,
-      text: 'Margarita',
+    text: "Negroni",
+  },
+  {
+    id: 2,
+    imageSrc: {
+      url: ABC,
+      alt: "Negroni Image",
     },
-    {
-      id: 3,
-      imageSrc: '',
-      text: ' Bellini',
+    text: "Margarita",
+  },
+  {
+    id: 3,
+    imageSrc: {
+      url: AJ,
+      alt: "Negroni Image",
     },
-    {
-      id: 4,
-      imageSrc: '',
-      text: 'Piña Colada',
+    text: " Bellini",
+  },
+  {
+    id: 4,
+    imageSrc: {
+      url: Ace,
+      alt: "Negroni Image",
     },
-    {
-      id: 5,
-      imageSrc: '',
-      text: 'Mimosa',
+    text: "Piña Colada",
+  },
+  {
+    id: 5,
+    imageSrc: {
+      url: ACID,
+      alt: "Negroni Image",
     },
-    {
-      id: 6,
-      imageSrc: '',
-      text: 'White Lady',
+    text: "Mimosa",
+  },
+  {
+    id: 6,
+    imageSrc: {
+      url: Affair,
+      alt: "Negroni Image",
     },
-    {
-      id: 7,
-      imageSrc: '',
-      text: 'Cosmopolitan',
+    text: "White Lady",
+  },
+  {
+    id: 7,
+    imageSrc: {
+      url: AmarettoSunrise,
+      alt: "Negroni Image",
     },
-    {
-      id: 8,
-      imageSrc: '',
-      text: 'Aviation',
+    text: "Cosmopolitan",
+  },
+  {
+    id: 8,
+    imageSrc: {
+      url: Apello,
+      alt: "Negroni Image",
     },
-    {
-      id: 9,
-      imageSrc: '',
-      text: 'Manhattan',
-    }
-  ];
-  
-  export default data;
-  
+    text: "Aviation",
+  },
+  {
+    id: 9,
+    imageSrc: {
+      url: ATT,
+      alt: "Negroni Image",
+    },
+    text: "Manhattan",
+  },
+];
+
+export default data;
